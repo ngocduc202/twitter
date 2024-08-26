9e6e5b937a0164d6a9a1faa90214406d7ed826b7
ngocduc202 <ngocduc1542002@gmail.com>
2024-08-26 23:21:49 +0700

    create userController

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import {v2 as cloudinary} from 'cloudinary'
 import authRoutes from './routers/auth.routes.js'
+import userRoutes from './routers/user.routes.js'
 import connectMongoDB from './db/connetMongoDb.js'
 import cookieParser from 'cookie-parser'
 
 dotenv.config()
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
 const app = express()
 const PORT = process.env.PORT || 5000
 
@@ -14,6 +23,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/user", userRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
