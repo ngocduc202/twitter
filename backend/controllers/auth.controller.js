c653350307f0017824d02b5f37b0c3b0d0c53081
ngocduc202 <ngocduc1542002@gmail.com>
2024-08-26 22:04:24 +0700

    create authController

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,17 +1,105 @@
+import { generateTokenAndSetCookie } from "../lib/utils/genarateToken.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-  res.json({
-    data: "signup"
-  })
+  try {
+    const {fullName , username , email , password} = req.body
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+			return res.status(400).json({ error: "Invalid email format" });
+		}
+
+    const existingUser = await User.findOne({ username });
+    if(existingUser){
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if(existingEmail){
+      return res.status(400).json({ error: "Email already exists" });
+    }
+
+    if(password.length < 6){
+      return res.status(400).json({ error: "Password must be at least 6 characters long" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashPassword
+    })
+
+    if(newUser){
+      generateTokenAndSetCookie(newUser._id, res)
+      await newUser.save()
+      res.status(200).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg
+      })
+    }else{
+      res.status(400).json({ error: "Failed to create user" })
+    }
+  } catch (error) {
+    console.log("Error in signup ", error)
+    res.status(500).json({ error: 'Internal server error' });
+  }
 }
 
 export const login = async (req, res) => {
-  res.json({
-    data: "login"
-  })
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+    if(!user || !isPasswordCorrect){
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    generateTokenAndSetCookie(user._id, res)
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      email: user.email,
+      followers: user.followers,
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg
+    })
+  } catch (error) {
+    console.log("Error in login ", error)
+    res.status(500).json({ error: 'Internal server error' });
+  }
 }
 
 export const logout = async (req, res) => {
-  res.json({
-    data: "logout"
-  })
+  try {
+    res.cookie("jwt","",{maxAge:0})
+    res.status(200).json({message:"Logged out successfully"})
+  } catch (error) {
+    console.log("Error in logout ", error)
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+export const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("Error in getMe ", error)
+    res.status(500).json({ error: 'Internal server error' });
+  }
 }
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routers/auth.routes.js'
 import connectMongoDB from './db/connetMongoDb.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 
