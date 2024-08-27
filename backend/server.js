49accd61ee49b640b2c0677202e1b75dcb0800c0
ngocduc202 <ngocduc1542002@gmail.com>
2024-08-27 14:27:07 +0700

    create postController

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import {v2 as cloudinary} from 'cloudinary'
 import authRoutes from './routers/auth.routes.js'
 import userRoutes from './routers/user.routes.js'
+import postRoutes from './routers/post.routes.js'
 import connectMongoDB from './db/connetMongoDb.js'
 import cookieParser from 'cookie-parser'
 
@@ -24,6 +25,7 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
+app.use("/api/posts", postRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
